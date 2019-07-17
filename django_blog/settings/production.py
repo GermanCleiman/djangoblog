7eeacbab4cd225be691fab508c6473d7a50d71cc
GermanCleiman <cleimangerman@gmail.com>
2019-07-17 15:22:05 -0300

    credenciales de base de datos heroku añadidas abolsadetrabajoescuela17

--- django_blog/settings/production.py
+++ django_blog/settings/production.py
@@ -13,7 +13,7 @@ DATABASES = {
         'NAME': 'dejoh5rgc6cj1o',
         'USER': 'qfofywzmuhjrwm',
         'PASSWORD': 'f9e9badc4b9996ab8dce819c006b26c5f886c3b665a7c9e5a67c2808f3c0dae8',
-        'POST':'ec2-174-129-227-80.compute-1.amazonaws.com',
+        'HOST':'ec2-174-129-227-80.compute-1.amazonaws.com',
         'PORT':5432,
     }
 }
