7b53145fe2310bb8989cfadecfd83e02a6514561
GermanCleiman <cleimangerman@gmail.com>
2019-07-17 17:01:15 -0300

    correccion1

--- django_blog/settings/production.py
+++ django_blog/settings/production.py
@@ -17,3 +17,7 @@ DATABASES = {
         'PORT':5432,
     }
 }
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
