5d2fd808010107a9680271625f45e8435add5605
GermanCleiman <cleimangerman@gmail.com>
2019-07-17 15:13:37 -0300

    credenciales de base de datos heroku añadidas abolsadetrabajoescuela17

--- django_blog/settings/production.py
+++ django_blog/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True                                                                   
+DEBUG = True
 
 ALLOWED_HOSTS = ['bolsadetrabajoescuela17.herokuapp.com']# Database
 
@@ -9,7 +9,11 @@ ALLOWED_HOSTS = ['bolsadetrabajoescuela17.herokuapp.com']# Database
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dejoh5rgc6cj1o',
+        'USER': 'qfofywzmuhjrwm',
+        'PASSWORD': 'f9e9badc4b9996ab8dce819c006b26c5f886c3b665a7c9e5a67c2808f3c0dae8',
+        'POST':'ec2-174-129-227-80.compute-1.amazonaws.com',
+        'PORT':5432,
     }
 }
