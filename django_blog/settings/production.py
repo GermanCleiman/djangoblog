39c5eee9bff99555d7f0d6e48dd23537d5e34928
GermanCleiman <cleimangerman@gmail.com>
2019-07-18 09:16:11 -0300

    modificacion 18dejulio 2

--- django_blog/settings/production.py
+++ django_blog/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = True
 
 ALLOWED_HOSTS = ['bolsadetrabajoescuela17.herokuapp.com']# Database
 
