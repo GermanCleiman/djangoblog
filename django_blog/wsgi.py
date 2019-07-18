29445ed5bc892efcf901fc51ad9cdc484bc5d4b4
GermanCleiman <cleimangerman@gmail.com>
2019-07-18 09:11:47 -0300

    modifucacion 18dejulio

--- django_blog/wsgi.py
+++ django_blog/wsgi.py
@@ -12,7 +12,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings.production')
 
 #application = get_wsgi_application()
 
